f81c18a6ed1019ef7ac0106541a7be4ccfc9bac3
Fredrik <35367024+fredrikaurdal@users.noreply.github.com>
2021-06-04 17:49:34 +0300

    Updated

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ function rotate() {
   let image = document.querySelector('.fixed-item .text');
   image.style.transform = 'rotate(' + window.pageYOffset / 20 + 'deg)';
 }
+
+const modal = document.querySelector('#modal');
+
+const button = document.querySelector('#modal-button');
+
+const span = document.getElementsByClassName('close')[0];
+
+button.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
